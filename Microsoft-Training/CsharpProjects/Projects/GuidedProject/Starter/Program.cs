fc2ed889279b22852146b562ff7b24f1c52c26ed
bofur24 <dransiktolkien@yahoo.com>
2023-11-16 12:41:17 -0600

    Added extra Credit check

--- Microsoft-Training/CsharpProjects/Projects/GuidedProject/Starter/Program.cs
+++ Microsoft-Training/CsharpProjects/Projects/GuidedProject/Starter/Program.cs
@@ -1,11 +1,11 @@
 ﻿using System;
 
 // initialize variables - graded assignments 
-int currentAssignments = 5;
-int[] sophiaScores = new int[] {90, 86, 87, 98, 100};
-int[] andrewScores = new int[] {92, 89, 81, 96, 90};
-int[] emmaScores = new int[] {90, 85, 87, 98, 68};
-int[] loganScores = new int[] {90, 95, 87, 88, 96};
+int examAssignments = 5;
+int[] sophiaScores = new int[] {90, 86, 87, 98, 100, 94, 90};
+int[] andrewScores = new int[] {92, 89, 81, 96, 90, 89};
+int[] emmaScores = new int[] {90, 85, 87, 98, 68, 89, 89, 89};
+int[] loganScores = new int[] {90, 95, 87, 88, 96, 96};
 
 string[] studentNames = new string[] {"Sophia", "Andrew", "Emma", "Logan"};
 int[] studentScores = new int[10];
@@ -28,14 +28,19 @@ foreach (string name in studentNames)
     
 
         int sumAssignmentScores = 0;
-        decimal currentStudentGrade;
+        decimal currentStudentGrade = 0;
+        int gradedAssignments = 0;
 
         foreach (int score in studentScores)
         {
-            sumAssignmentScores += score;        
+            gradedAssignments += 1;
+            if (gradedAssignments <= examAssignments)
+                sumAssignmentScores += score; 
+            else
+                sumAssignmentScores += score / 10;   
         }
 
-        currentStudentGrade = (decimal)sumAssignmentScores / currentAssignments;
+        currentStudentGrade = (decimal)sumAssignmentScores / examAssignments;
 
         if (currentStudentGrade >= 97)
             currentStudentLetterGrade = "A+";
